616c20017cc1aef289c4cb0367f5074734d1c0d4
codeask10 <annajmussaquib123@gmail.com>
2024-03-07 17:32:53 +0530

    changes

--- server/Database.js
+++ server/Database.js
@@ -4,12 +4,11 @@ dotenv.config();
 const mongoURL=process.env.REACT_APP_MONGOURL;
 console.log(mongoURL);
  const connectToMongoose=()=>{
-    mongoose.connect(mongoURL)
-    .then(()=>{
-        console.log("MongoDB Connected");
-    })
-    .catch((err)=>{
-        console.error("MongoDb connection error",err);
-    });
+    
+mongoose.connect(
+    mongoURL
+  )
+  .then(()=>console.log('connected'))
+  .catch(e=>console.log(e));
  }
 module.exports=connectToMongoose;
